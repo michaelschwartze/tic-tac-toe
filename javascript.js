c58ee2a9e3ea71fd40cc30443b6b53e8523722eb
Michael Schwartze <mschwartze@gmail.com>
2016-11-24 21:41:34 -0500

    Check for win rows 1-3, both X and O

--- javascript.js
+++ javascript.js
@@ -1,17 +1,33 @@
 $(function() {
 
   var turn = 0;
+  var gameStatus = 'playing';
+
   var clearMsg = function() {
     $('#msg-text').text("")
   };
 
-  var gameStatus = 'playing';
+  function areEqual() {
+    var len = arguments.length;
+    for (var i = 1; i< len; i++){
+      if (arguments[i] == null || arguments[i] != arguments[i-1] || arguments[i] === "")
+        return false;
+    }
+    return true;
+  };
+
+  console.log(areEqual(1,1,1,1,3,1));
 
   var checkForWin = function(){
-    var row = $('*[data-row-num=1] td').text();
-    if (row === 'XXX') {
-      $('#msg-text').text('X Wins!');
-      gameStatus = 'over';
+    for (var i = 0; i < 4; i++) {
+      var row = $('*[data-row-num=' + (i + 1) + '] td').text();
+      var a = row.substr(0,1);
+      var b = row.substr(1,1);
+      var c = row.substr(2,1);
+      if (areEqual(a,b,c) === true) {
+        $('#msg-text').text(row.substr(0,1)+ ' Wins!');
+        gameStatus = 'over';
+      }
     }
   };
 
@@ -35,6 +51,8 @@ $(function() {
   $('button').on('click', function() {
     $('.table-board td').empty();
     clearMsg();
+    gameStatus = 'playing';
+    turn = 0;
   });
 
 });
