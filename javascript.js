1195b9afb7444cde5ea98b6a5fcbf769aabbbfa8
Michael Schwartze <mschwartze@gmail.com>
2016-11-24 19:44:50 -0500

    Don't allow values to change once cell has been marked

--- javascript.js
+++ javascript.js
@@ -1,16 +1,21 @@
 $(function() {
   var turn = 0;
   $('td').on('click', function() {
-    if (turn % 2 === 0) {
-    $(this).text('X');
-    } else {
-    $(this).text('O');
+
+    if ($(this).text() === "") {
+      if (turn % 2 === 0) {
+      $(this).text('X');
+      } else {
+      $(this).text('O');
+      };
+      turn ++; } else {
     };
-    turn ++;
+  });
+
+  $('button').on('click', function() {
+    $('.table-board td').empty();
   });
 
 });
 
-$('button').on('click', function() {
-  $('.table-board td').empty();
-})
+// don't allow the value to change if it isn't clear
